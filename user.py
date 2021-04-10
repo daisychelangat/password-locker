62e4ee52bab3931229a3fcd6354be552d4558de9
daisychelangat <daisyrop2000@gmail.com>
2021-04-11 00:09:59 +0300

    created a tes_save_user to confirm addition of users in the user list

--- user.py
+++ user.py
@@ -7,3 +7,8 @@ class User:
     def __init__(self,username,password):
         self.username = username
         self.password = password
+    def save_user(self):
+        '''
+        save_user method saves a new user object to user list
+        '''
+        User.user_list.append(self)
--- user_test.py
+++ user_test.py
@@ -22,6 +22,13 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.username,'daisychelangat')
         self.assertEqual(self.new_user.password,'284565q')
 
+    def test_save_user(self):
+        '''
+        test case to check if the new instance of the user object has been created
+        '''
+        
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
 
 
 if __name__ == '__main__':
