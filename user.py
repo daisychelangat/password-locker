73d0aaca54aecde36fdfa15e999f0d02b919ec5d
daisychelangat <daisyrop2000@gmail.com>
2021-04-11 14:51:00 +0300

    created a new file run.py

--- user.py
+++ user.py
@@ -18,9 +18,9 @@ class User:
         '''
         User.user_list.remove(self)
 
-#    @classmethod
-#     def display_user(cls):
-#         return cls.user_list
+   @classmethod
+    def display_user(cls):
+        return cls.user_list
 class Credentials:
     credentials_list = []
     def __init__(self,account,username,password):
@@ -28,15 +28,50 @@ class Credentials:
         self.password = password
         self.username = username
 
-#  @classmethod
-#     def verify_user(cls,username,password):
-#         a_user = ''
-#         for user in User.user_list:
-#             if(user.username == username and user.password == password):
-#                 a_user == user.username
-#                 return a_user
+ @classmethod
+    def verify_user(cls,username,password):
+        a_user = ''
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                a_user == user.username
+                return a_user
     def save_user_credentials(self):
         '''
         save_user_credential method saves a new user object to credentials list
         '''
         Credentials.credentials_list.append(self)
+
+        def delete_credentials(self):
+        '''
+        delete saved credentials in the credentials list
+        '''
+        Credentials.credentials_list.remove(self)
+    @classmethod
+    def find_by_number(cls,account):
+        '''
+        this method takes in password and returns a password that match that number 
+        Args:
+        number: password number to search for
+        Returns :
+        password of person that matches the number.
+        '''
+        for credential in cls.credentials_list:
+            if(credential.account == account):
+                return credential
+    @classmethod
+    def credentials_exist(cls,account):
+        '''
+        this method checks whether the user details exists from the user list
+        it returns a boolean on availability or not i.e True|False
+        '''
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return True
+        return False
+    def generate_password(self):
+        '''
+        generate random password consisting of letters
+        '''
+        password = string.ascii_uppercase + string.ascii_lowercase
+        return ''.join(random.choice(password) for i in range(1,9))
+    
--- user_test.py
+++ user_test.py
@@ -14,14 +14,13 @@ class TestUser(unittest.TestCase):
         """
         a method that runs before the test
         """
-        self.new_user = User('daisychelangat','284565q')
+        self.new_user = User('daisychelangat','200084565q')
     def test_init(self):
         """
         test case to check if the object in initialized correctly
         """
-      
-        self.assertEqual(self.new_user.username,'daisychelangat')
-        self.assertEqual(self.new_user.password,'284565q')
+       self.assertEqual(self.new_user.username,'daisychelangat')
+       self.assertEqual(self.new_user.password,'200084565q')
 
     def test_save_user(self):
         '''
@@ -39,7 +38,7 @@ class TestCredentials(unittest.TestCase):
         '''
         Method that runs before each individual credentials test methods run.
         '''
-        self.new_credentials = Credentials('dee','daisychelangat','284565q')
+        self.new_credentials = Credentials('dee','daisychelangat','200084565q')
 
     def tearDown(self):
         '''
@@ -54,7 +53,7 @@ class TestCredentials(unittest.TestCase):
         
         self.assertEqual(self.new_credentials.account,'dee')
         self.assertEqual(self.new_credentials.username,'daisychelangat')
-        self.assertEqual(self.new_credentials.password,'284565q') 
+        self.assertEqual(self.new_credentials.password,'200084565q') 
 
     def test_save_credentials(self):
         """
@@ -62,6 +61,53 @@ class TestCredentials(unittest.TestCase):
         """
         self.new_credentials.save_user_credentials()
         self.assertEqual(len(Credentials.credentials_list),1)
+
+    
+    def test_save_many_account(self):
+        '''
+        test to check if we can save multiple credentials objects to our credentials list
+        '''
+        self.new_credentials.save_user_credentials()
+        test_creditial = Credentials('dee','daisychelangat','200084565q')
+        test_creditial.save_user_credentials()
+        self.assertEqual(len(Credentials.credentials_list),2)
+
+    def test_find_creditial(self):
+        """
+        test to check if we can find a credential entry by account name and display the details of the credential
+        """
+        
+        self.new_credentials.save_user_credentials()
+        test_creditial = Credentials('dee','daisychelangat','200084565q')
+        test_creditial.save_user_credentials()
+
+        the_creditial = Credentials.find_by_number("dee")
+        self.assertEqual(the_creditial.account,test_creditial.account)
+
+    def test_creditial_exist(self):
+        """
+        test to check if we can return a true or false based on whether we find or can't find the credential.
+        """
+        self.new_credentials.save_user_credentials()
+        test_creditial = Credentials('dee','daisychelangat','200084565q')
+        test_creditial.save_user_credentials()
+        
+        found_credential = Credentials.credentials_exist("dee")
+        self.assertTrue(found_credential)
+
+    def test_delete_credential(self):
+        """
+        test method to test if we can remove an account credentials from our credentials_list
+        """
+        self.new_credentials.save_user_credentials()
+        test_creditial = Credentials('dee','daisychelangat','200084565q')
+        test_creditial.save_user_credentials()
+        
+        
+        self.new_credentials.delete_credentials()
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+
 
 
 if __name__ == '__main__':
